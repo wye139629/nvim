5aaf8a600b5264223eb7b6558a11dd5b2040f6b8
William Liu <wye139629@gmail.com>
2023-08-03 18:26:55 +0800

    feat: add fugitive

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -37,3 +37,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- lazygit
+vim.keymap.set("n", '<leader>gg' , "<cmd>!lazygit<CR>")
+
