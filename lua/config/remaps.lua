8318bb1947c2195e6aa31d5072f463191d9b8272
William Liu <wye139629@gmail.com>
2023-08-03 17:38:29 +0800

    feat: add autotag autopair and emmet

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- Lsp format
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "FF", vim.lsp.buf.format)
 
 -- quick fix navigation
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
@@ -30,12 +30,10 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
--- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 -- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Source 
+-- Source
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+    dependencies = { 'windwp/nvim-ts-autotag', { 'windwp/nvim-autopairs', config = true } },
 	build = function()
 		require("nvim-treesitter.install").update({ with_sync = true })
 	end
